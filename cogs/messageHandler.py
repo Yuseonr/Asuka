86ee98808efd397419f2ecd9c75075b0986324b6
Yuseonr <rafianandra@gmail.com>
2026-03-20 21:30:30 +0700

    feat : message handler cuba cuba

---
+++ cogs/messageHandler.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+
+class messageHandler(commands.Cog):
+    """ Handle incoming message for Asuka """
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+        
+        if "asuka" in message.content.lower():
+            await message.channel.send(f"Hmph u called me? what do you want, <@{message.author.id}>?")
+        
+        elif message.content.lower() == "good night":
+            await message.channel.send("Hmph, whatever. Make sure your code actually compiles before you sleep.")
+    
+
+async def setup(bot):
+    await bot.add_cog(messageHandler(bot))
